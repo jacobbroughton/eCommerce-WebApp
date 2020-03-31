df6c5712fb7f7df41a2f412d0d9279f2b3d6f63b
jlb1999 <jlbroughton88@gmail.com>
2020-03-31 10:05:44 -0400

    profile loads listings correctly now

--- client/src/components/ProfileListings/ProfileListings.js
+++ client/src/components/ProfileListings/ProfileListings.js
@@ -1,37 +1,34 @@
-import React, {useEffect, useState} from "react";
-import {useAuth0} from "../../contexts/auth0-context";
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "../../contexts/auth0-context";
 import axios from "axios";
 import "./ProfileListings.scss";
 
-
-
 const ProfileListings = () => {
+  const { dbUser } = useAuth0();
+  const [listings, setListings] = useState([]);
 
-const {dbUser} = useAuth0();
-const [listings, setListings] = useState([])
-
-const getListings = () => {
+  const getListings = () => {
     axios
-    .get(`http://localhost:5000/api/personallistings/${dbUser.user_uid}`)
-    .then(response => setListings([...response.data]))
-    .catch(err => console.log(err))
-}
+      .get(`http://localhost:5000/api/personallistings/${dbUser.user_uid}`)
+      .then(response => setListings([...response.data]))
+      .catch(err => console.log(err));
+  };
 
-useEffect(() => {
+  useEffect(() => {
     getListings();
-}, [dbUser])
+  }, [listings]);
 
-    return (
-        <div className="profileListingsMother">
-            {listings.map(listing => 
-                <div className="listingItem">
-                    <h3>{listing.title}</h3>
-            <p>{listing.category}</p>
-            <p>{listing.price}</p>
-                </div>
-                )}
+  return (
+    <div className="profileListingsMother">
+      {listings.map(listing => (
+        <div key={listing.id} className="listingItem">
+          <h3>{listing.title}</h3>
+          <p>{listing.category}</p>
+          <p>{listing.price}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default ProfileListings;
