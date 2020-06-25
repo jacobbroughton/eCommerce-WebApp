fb1e7c5b03aa262f77bcc3ecd0b99109e6efb7e2
jlb1999 <jlbroughton88@gmail.com>
2020-06-25 08:47:17 -0400

    test

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server listening at port 5000");
+  console.log(`Server listening at port ${port}`);
 });
